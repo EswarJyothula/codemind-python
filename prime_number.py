7c4e95b559614513a9bcf98d34ba21e7b6c87baf
EswarJyothula <98584786+EswarJyothula@users.noreply.github.com>
2023-06-01 10:27:03 +0530

    def isprime(n):     for i in range(2,int(n**0.5)+1):         if n%i==0:             return False     return True n=int(input()) if isprime(n):     print("prime") else:     print("not a prime")

---
+++ prime_number.py
@@ -0,0 +1,10 @@
+def isprime(n):
+    for i in range(2,int(n**0.5)+1):
+        if n%i==0:
+            return False
+    return True
+n=int(input())
+if isprime(n):
+    print("prime")
+else:
+    print("not a prime")
